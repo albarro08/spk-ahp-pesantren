b7e7c6af4aabc91b1eb2a210c603e41fd3c6853a
Bara Alim <alimbara50@gmail.com>
2026-06-04 11:32:31 +0700

    Debug ailway DB

--- config/database.js
+++ config/database.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql2');
 
+console.log('DB_HOST =', process.env.DB_HOST);
+console.log('DB_USER =', process.env.DB_USER);
+console.log('DB_NAME =', process.env.DB_NAME);
+
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
